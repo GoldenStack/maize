273139fa15dc96272dac08dcc0c1eb28e3d4c8f9
GoldenStack <goldenfire64yt@gmail.com>
2024-03-24 00:44:44 -0500

    Document parsing

--- src/ast.rs
+++ src/ast.rs
@@ -156,36 +156,43 @@ impl Expr {
         let token = real_token(input)?;
 
         match token.as_str() {
-            _ if is_infix(&token) => {
+            _ if is_infix(&token) => { // Do not allow standalone infixes
                 *input = old_input;
                 return Err((input.to_owned(), format!("Expected operation, found infix '{}'", token)));
             },
-            ")" => {
+            ")" => { // Do not allow standalone closing parentheses
                 *input = old_input;
                 return Err((input.to_owned(), "Found closing parentheses".to_owned()));
             },
-            "(" => {
+            "(" => { // Parse opening parentheses
+                // Allow any expression within them
                 let infix = Expr::parse(input)?;
     
                 ws(input)?;
+                // Require closing parentheses
                 if let Err(_) = require(")", input) {
                     return Err((input.to_owned(), "Expected closing parentheses".to_owned()));
                 }
     
                 return Ok(infix);
             },
-            _ => Ok(Expr::Name(token)),
+            _ => Ok(Expr::Name(token)), // Otherwise it's a 
         }
     }
  
     pub fn parse_prefix(mut left: Expr, input: &mut &str) -> PResult<Expr> {
         loop {
+            // If there isn't another basic expression, this means it's not a
+            // prefix operation, so we can exit.
             let Ok(right) = Expr::parse_basic(input) else {
                 return Ok(left);
             };
 
             let order = get_associativity(leftmost(&left), leftmost(&right));
 
+            // If it's right associative relative to the left element we pair it
+            // up with the element after it. Otherwise we ignore the element
+            // after and allow following loop iterations to handle it.
             left = Expr::app(left, match order {
                 Associativity::Right => Expr::parse_prefix(right, input)?,
                 Associativity::Left => right
@@ -200,21 +207,30 @@ impl Expr {
         }
 
         loop {
+            // If we can't find an infix operator, this means it's not an infix
+            // expression, so we can exit.
             let Some(infix) = peek_infix(input) else {
                 return Ok(left);
             };
             real_token(input)?;
 
+            // Parse the left part and the infix part (but flipped, as is infix)
             let left_and_infix = Expr::app(Expr::Name(infix), left);
 
+            // Append the right half 
             let all = Expr::parse_prefix(left_and_infix, input)?;
 
+            // Try to find another infix
             let Some(next) = peek_infix(input) else {
                 return Ok(all);
             };
 
             let order = get_associativity(leftmost(&all), &next);
 
+            // If there's another infix with right associativity relative to the
+            // current expression, let it take the right element from this one.
+            // Otherwise, fall back and allow following loop iterations to
+            // to handle it.
             left = match order {
                 Associativity::Right => {
                     match all {
@@ -228,6 +244,11 @@ impl Expr {
     }
 
     pub fn parse(input: &mut &str) -> PResult<Expr> {
+        // Parsing occurs down the parse tree:
+        // Infix -> Prefix -> Basic
+
+        // We replicate the first version of that here as it makes other parts
+        // of the implementation much simpler.
         Expr::parse_infix(Expr::parse_prefix(Expr::parse_basic(input)?, input)?, input)
     }
 
