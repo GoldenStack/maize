0c84143f0564871ff26d7bdf3a2ae3b8aaedbd20
GoldenStack <goldenfire64yt@gmail.com>
2024-10-04 16:33:24 -0500

    De Bruijn indices partial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 pub mod parse;
+pub mod process;
 pub mod infer;
+
+use process::process;
 
 use crate::parse::{parse, Context, Reader};
 
 fn main() {
-    let input = "(\\x \\y y) 1 2";
+    let input = "(\\x x \\y \\x x y)) 1 2";
 
     let context = Context::default();
 
     let mut input = Reader::new(input);
-
-
     println!("Parsing:  {:?}", input);
-    println!("Yields:   {}", parse(&context, &mut input).unwrap());
+
+    let mut parsed = parse(&context, &mut input).unwrap();
+    println!("Yields:   {}", parsed);
+
+
+    let mut processed = process(parsed);
+    println!("Processed: {}", processed);
 }
